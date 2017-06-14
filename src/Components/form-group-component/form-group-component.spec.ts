acbd8bcdef198be98c248c90868233dcf02e8ab7
Kevin Kipp <kevin.kipp@gmail.com>
2017-06-13 23:01:37 -0500

    Fix form group component test

--- src/Components/form-group-component/form-group-component.spec.ts
+++ src/Components/form-group-component/form-group-component.spec.ts
@@ -1,25 +1,39 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from "@angular/core/testing";
 
-import { FormGroupComponent } from './form-group-component';
+import {FormGroupComponent} from "./form-group-component";
+import {ErrorMessageService} from "../../Services/error-message.service";
+import {CUSTOM_ERROR_MESSAGES} from "../../Tokens/tokens";
+import {errorMessageService} from "../../ng-bootstrap-form-validation.module";
 
 describe('FormGroupComponent', () => {
-  let component: FormGroupComponent;
-  let fixture: ComponentFixture<FormGroupComponent>;
+    let component: FormGroupComponent;
+    let fixture: ComponentFixture<FormGroupComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FormGroupComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [FormGroupComponent],
+            providers: [
+                {
+                    provide: ErrorMessageService,
+                    useFactory: errorMessageService,
+                    deps: [CUSTOM_ERROR_MESSAGES]
+                },
+                {
+                    provide: CUSTOM_ERROR_MESSAGES,
+                    useValue: []
+                }
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FormGroupComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FormGroupComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
