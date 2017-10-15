d74883bf483b3529f29fa7b7ef641f56e4bd203c
Kevin Kipp <kevin.kipp@gmail.com>
2017-10-15 10:11:26 -0500

    Mess with spacing

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AppPage } from './app.po';
+import { AppPage } from "./app.po";
 
-describe('ng-bootstrap-form-validation App', () => {
+describe("ng-bootstrap-form-validation App", () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it("should display welcome message", () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual("Welcome to app!");
   });
 });
--- src/lib/public_api.ts
+++ src/lib/public_api.ts
@@ -1,4 +1,11 @@
-export {errorMessageServiceFactory, NgBootstrapFormValidationModule} from './ng-bootstrap-form-validation.module'
-export {ErrorMessage, FormatErrorFunction} from './Models/ErrorMessage'
-export {FormValidationDirective} from './Directives/form-validation.directive'
-export {FormGroupComponent} from './Components/form-group-component/form-group-component'
+export {
+  errorMessageServiceFactory,
+    NgBootstrapFormValidationModule
+} from "./ng-bootstrap-form-validation.module";
+export { ErrorMessage, FormatErrorFunction } from "./Models/ErrorMessage";
+export {
+  FormValidationDirective
+} from "./Directives/form-validation.directive";
+export {
+  FormGroupComponent
+} from "./Components/form-group-component/form-group-component";
