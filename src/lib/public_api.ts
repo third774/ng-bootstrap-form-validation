861b2d869d2b2bd96b55a931ac7d20d8f2e064dd
Kevin Kipp <kevin.kipp@gmail.com>
2017-10-15 10:09:08 -0500

    Move public_api.ts into src/lib

---
+++ src/lib/public_api.ts
@@ -0,0 +1,4 @@
+export {errorMessageServiceFactory, NgBootstrapFormValidationModule} from './ng-bootstrap-form-validation.module'
+export {ErrorMessage, FormatErrorFunction} from './Models/ErrorMessage'
+export {FormValidationDirective} from './Directives/form-validation.directive'
+export {FormGroupComponent} from './Components/form-group-component/form-group-component'
