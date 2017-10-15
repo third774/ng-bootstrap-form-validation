b9590c5c3268eb1018c887c3d2b86ae11679da9b
Kevin Kipp <kevin.kipp@gmail.com>
2017-10-15 11:25:59 -0500

    Remove barrel export files

--- src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-export {
-  errorMessageServiceFactory,
-  NgBootstrapFormValidationModule
-} from "./ng-bootstrap-form-validation.module";
-export { ErrorMessage, FormatErrorFunction } from "./Models/ErrorMessage";
