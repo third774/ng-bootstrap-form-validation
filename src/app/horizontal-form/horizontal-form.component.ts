520d98feb68ef43a36388c3243db06593cf57645
Kevin Kipp <kevin.kipp@gmail.com>
2017-10-15 20:10:37 -0500

    Add horizontal form

--- src/app/horizontal-form/horizontal-form.component.ts
+++ src/app/horizontal-form/horizontal-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-horizontal-form",
@@ -6,7 +7,17 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./horizontal-form.component.css"]
 })
 export class HorizontalFormComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl("", Validators.email),
+    password: new FormControl("", Validators.required),
+    rememberMe: new FormControl(false)
+  });
+
   constructor() {}
 
   ngOnInit() {}
+
+  handleSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
