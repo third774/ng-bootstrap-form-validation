f263871c158054ed700f6d5bfa5a5dab6f70b143
Kevin Kipp <kevin.kipp@gmail.com>
2017-10-18 19:39:32 -0500

    Add generateErrorMessages to error message service

--- src/lib/Services/error-message.service.ts
+++ src/lib/Services/error-message.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from "@angular/core";
 import { ErrorMessage } from "../Models/ErrorMessage";
 import { DEFAULT_ERRORS } from "../default-errors";
 import { CUSTOM_ERROR_MESSAGES } from "../Tokens/tokens";
+import { NgControl } from "@angular/forms";
 
 @Injectable()
 export class ErrorMessageService {
@@ -14,5 +15,36 @@ export class ErrorMessageService {
 
   get errorMessages() {
     return [...this.customErrorMessages, ...this.defaultErrors];
+  }
+
+  private mergeErrorMessages(
+    formControlCustomErrorMessages: ErrorMessage[]
+  ): ErrorMessage[] {
+    return [
+      ...formControlCustomErrorMessages,
+      ...this.customErrorMessages,
+      ...this.defaultErrors
+    ];
+  }
+
+  public generateErrorMessages(
+    formControl: NgControl,
+    label: string,
+    formControlErrorMessages: ErrorMessage[] = []
+  ): string[] {
+    if (formControl.valid) return [];
+    const mergedErrorMessages = this.mergeErrorMessages(
+      formControlErrorMessages
+    );
+    return Object.keys(
+      formControl.errors
+    ).reduce((messages: string[], key: string) => {
+      const error = mergedErrorMessages.find(
+        errorMessage => errorMessage.error === key
+      );
+      if (!error) return messages;
+      messages.push(error.format(label, formControl.errors[key]));
+      return messages;
+    }, []);
   }
 }
