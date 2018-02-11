a437ae1e9bf67b3bfc682fdcae27cf59bf959f56
Kevin Kipp <kevin.kipp@gmail.com>
2018-02-11 09:11:41 -0600

    Debounce invalid feedback and support async
    This will debounce error messages. Once they are shown, they will continue to be
    shown until a valid state is reached. At that point, we change the eagerValidation
    flag to debounce feedback again.
    
    Additionally, this also adds support for showing the `has-warning` class
    when the status is pending for async validators.

--- src/lib/Components/form-group-component/form-group-component.ts
+++ src/lib/Components/form-group-component/form-group-component.ts
@@ -6,12 +6,22 @@ import {
   HostBinding,
   Input,
   QueryList,
-  AfterContentInit
+  AfterContentInit,
+  Inject,
+  Host,
+  Optional,
+  OnDestroy
 } from "@angular/core";
 import { FormControlName } from "@angular/forms";
 import { ErrorMessage } from "../../Models/ErrorMessage";
 import { ErrorMessageService } from "../../Services/error-message.service";
 import { MessagesComponent } from "../messages/messages.component";
+import { FormValidationDirective } from "../../Directives";
+
+import "rxjs/add/operator/filter";
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/debounceTime";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   // tslint:disable:component-selector
@@ -21,7 +31,7 @@ import { MessagesComponent } from "../messages/messages.component";
     <bfv-messages *ngIf="!messagesBlock" [messages]="messages"></bfv-messages>
   `
 })
-export class FormGroupComponent implements AfterContentInit {
+export class FormGroupComponent implements AfterContentInit, OnDestroy {
   @ContentChildren(FormControlName)
   FormControlNames: QueryList<FormControlName>;
 
@@ -32,7 +42,9 @@ export class FormGroupComponent implements AfterContentInit {
   @HostBinding("class.has-error")
   get hasErrors() {
     return (
-      this.FormControlNames.some(c => !c.valid && c.dirty && c.touched) &&
+      this.showFeedback &&
+      this.FormControlNames.some(c => c.invalid) &&
+      this.FormControlNames.some(c => c.dirty) &&
       !this.validationDisabled
     );
   }
@@ -40,27 +52,77 @@ export class FormGroupComponent implements AfterContentInit {
   @HostBinding("class.has-success")
   get hasSuccess() {
     return (
+      this.showFeedback &&
       !this.FormControlNames.some(c => !c.valid) &&
-      this.FormControlNames.some(c => c.dirty && c.touched) &&
+      this.FormControlNames.some(c => c.dirty) &&
+      !this.validationDisabled
+    );
+  }
+
+  @HostBinding("class.has-warning")
+  get hasWarning() {
+    return (
+      this.FormControlNames.some(c => c.pending) &&
+      this.FormControlNames.some(c => c.dirty) &&
       !this.validationDisabled
     );
   }
 
   @ContentChild(MessagesComponent) public messagesBlock: MessagesComponent;
 
+  private showFeedback = false;
   public messages: () => string[];
+  private eagerValidation = true;
+  private subscriptions: Subscription[] = [];
 
   constructor(
     private elRef: ElementRef,
+    @Optional()
+    @Host()
+    @Inject(FormValidationDirective)
+    private form: FormValidationDirective,
     private errorMessageService: ErrorMessageService
   ) {
     this.messages = () => this.getMessages();
   }
 
   ngAfterContentInit() {
+    if (this.form) {
+      this.subscriptions.push(
+        this.form.anySubmit.subscribe(() => (this.showFeedback = true))
+      );
+    }
+    this.FormControlNames.forEach(c =>
+      this.subscriptions.push(
+        c.statusChanges
+          .filter(status => status === "INVALID")
+          .do(() => {
+            if (!this.eagerValidation) {
+              this.showFeedback = false;
+            }
+          })
+          .debounceTime(500)
+          .subscribe(() => {
+            this.eagerValidation = true;
+            this.showFeedback = true;
+          })
+      )
+    );
+    this.FormControlNames.forEach(c =>
+      this.subscriptions.push(
+        c.statusChanges.filter(status => status === "VALID").subscribe(() => {
+          this.eagerValidation = false;
+          this.showFeedback = true;
+        })
+      )
+    );
     if (this.messagesBlock) {
       this.messagesBlock.messages = this.messages;
     }
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
   get label() {
@@ -80,13 +142,16 @@ export class FormGroupComponent implements AfterContentInit {
 
   private getMessages(): string[] {
     const messages = [];
-    if (!this.isDirtyAndTouched || this.validationDisabled) {
+    if (
+      !this.showFeedback ||
+      !this.FormControlNames.some(c => c.dirty) ||
+      this.validationDisabled
+    ) {
       return messages;
     }
 
-    this.FormControlNames
-      .filter(c => !c.valid && !!c.errors)
-      .forEach(control => {
+    this.FormControlNames.filter(c => !c.valid && !!c.errors).forEach(
+      control => {
         Object.keys(control.errors).forEach(key => {
           const error = this.errorMessages.find(err => err.error === key);
           if (!error) {
@@ -94,7 +159,8 @@ export class FormGroupComponent implements AfterContentInit {
           }
           messages.push(error.format(this.label, control.errors[key]));
         });
-      });
+      }
+    );
 
     return messages;
   }
