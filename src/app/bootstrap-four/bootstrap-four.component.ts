83b6befc98e7e3e2fe3ac09e1eeedbfc2776e722
Kevin Kipp <kevin.kipp@gmail.com>
2018-07-15 13:46:09 -0500

    Fetch styles on mount

--- src/app/bootstrap-four/bootstrap-four.component.ts
+++ src/app/bootstrap-four/bootstrap-four.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 
 @Component({
   selector: "app-bootstrap-four",
   templateUrl: "./bootstrap-four.component.html",
   styleUrls: ["./bootstrap-four.component.css"]
 })
-export class BootstrapFourComponent implements OnInit {
+export class BootstrapFourComponent implements OnInit, OnDestroy {
+  link: HTMLLinkElement;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.link = document.createElement("link");
+    this.link.href =
+      "https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css";
+    this.link.rel = "stylesheet";
+    this.link.integrity =
+      "sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B";
+    this.link.crossOrigin = "anonymous";
+    document.head.appendChild(this.link);
+  }
+
+  ngOnDestroy() {
+    document.head.removeChild(this.link);
+  }
 }
--- src/app/bootstrap-three/bootstrap-three.component.ts
+++ src/app/bootstrap-three/bootstrap-three.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 
 @Component({
   selector: "app-bootstrap-three",
   templateUrl: "./bootstrap-three.component.html",
   styleUrls: ["./bootstrap-three.component.css"]
 })
-export class BootstrapThreeComponent implements OnInit {
+export class BootstrapThreeComponent implements OnInit, OnDestroy {
+  link: HTMLLinkElement;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.link = document.createElement("link");
+    this.link.href =
+      "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
+    this.link.rel = "stylesheet";
+    this.link.integrity =
+      "sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u";
+    this.link.crossOrigin = "anonymous";
+    document.head.appendChild(this.link);
+  }
+
+  ngOnDestroy() {
+    document.head.removeChild(this.link);
+  }
 }
