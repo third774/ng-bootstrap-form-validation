15f01caab965de02f8a1a8d799319f165a75ce93
Kevin Kipp <kevin.kipp@gmail.com>
2018-07-15 22:28:28 -0500

    Wire up bootstrap 4 form

--- src/app/bootstrap-four-basic/bootstrap-four-basic.component.ts
+++ src/app/bootstrap-four-basic/bootstrap-four-basic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-bootstrap-four-basic",
@@ -6,7 +7,28 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./bootstrap-four-basic.component.css"]
 })
 export class BootstrapFourBasicComponent implements OnInit {
+  formGroup = new FormGroup({
+    firstName: new FormControl("", Validators.required),
+    lastName: new FormControl("", Validators.required),
+    email: new FormControl("", Validators.email),
+    city: new FormControl("", Validators.required),
+    state: new FormControl("", Validators.required),
+    zip: new FormControl("", Validators.required),
+    agreeToTerms: new FormControl(false, Validators.required),
+    bestPet: new FormControl(null, Validators.required)
+  });
+
   constructor() {}
 
   ngOnInit() {}
+
+  handleValidSubmit() {
+    console.log(this.formGroup.value);
+  }
+
+  handleReset() {
+    this.formGroup.reset();
+  }
 }
+
+// .custom-control-input.is-invalid~.custom-control-label
