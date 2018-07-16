7c97c050f305b3358133ade81e7983c4b5c3472b
Kevin Kipp <kevin.kipp@gmail.com>
2018-07-15 22:53:10 -0500

    Remove unused import

--- src/lib/Components/messages/messages.component.ts
+++ src/lib/Components/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Inject } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { ErrorMessageService } from "../../Services/error-message.service";
 
 @Component({
