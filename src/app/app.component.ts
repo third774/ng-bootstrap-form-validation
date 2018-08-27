3390f5b9dc6f28a946d4663594db18c339a233ca
Joel Muskwe <joel.muskwe@gmail.econet.co.zw>
2018-08-27 09:16:01 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bfv-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ngx-bootstrap-form-validation';
+}
